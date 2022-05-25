21e6aa053e0ee71836cf183bd0ee3ee901dd9d19
mohamadreza mirshamsi <m_mirshamsi@comp.iust.ac.ir>
2022-05-25 17:45:52 +0430

    add register api

--- app/account/urls.py
+++ app/account/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from account.views import *
 
 urlpatterns = [
+    path('register', Register.as_view()),
     path('login', Login.as_view()),
 ]
--- app/account/views.py
+++ app/account/views.py
@@ -5,6 +5,25 @@ from rest_framework.permissions import IsAuthenticated
 from account.models import *
 
 
+class Register(APIView):
+    def post(self, req):
+        data = req.data
+        user = User.objects.filter(username=data.get('username')).first()
+        if user:
+            return Response({'code':102, 'message':'already registered'}, status=422)
+        if not data.get('username') or not data.get('password'):
+            return Response({'code':103, 'message':'invalide data'}, status=422)
+        user = User.objects.create(
+            username = data.get('username'),
+            first_name = data.get('first_name'),
+            last_name = data.get('last_name')
+        )
+        user.set_password(data['password'])
+        refresh, access = user.new_token()
+        return Response({'code':100, 'message':'', 'data':{'refresh_token':refresh, 'access_token':f'Bearer {access}'}})
+        
+
+
 class Login(APIView):
     def post(self, req):
         data = req.data
