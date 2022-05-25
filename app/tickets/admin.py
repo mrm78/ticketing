0db012f583b4506fb5d6d9c467994b63e3be5a83
mohamadreza mirshamsi <m_mirshamsi@comp.iust.ac.ir>
2022-05-25 18:42:08 +0430

    add ticketing apis

--- app/tickets/admin.py
+++ app/tickets/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from tickets.models import *
 
-# Register your models here.
+
+admin.site.register(TicketSubject)
+admin.site.register(Ticket)
+admin.site.register(TicketMessage)
--- app/tickets/urls.py
+++ app/tickets/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from tickets.views import *
 
 urlpatterns = [
-
+    path('create_ticket', CreateTicket.as_view()),
+    path('answer_ticket', AnswerTicket.as_view()),
+    path('ticket_subjects', TicketSubjects.as_view()),
+    path('user_tickets', UserTickets.as_view()),
+    path('ticket_chats', TicketChats.as_view()),
 ]
