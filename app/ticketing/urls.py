0a519764355fe323b37f541d11fafea18bfb270e
mohamadreza mirshamsi <m_mirshamsi@comp.iust.ac.ir>
2022-05-25 17:49:41 +0430

    init tickets app

--- app/ticketing/urls.py
+++ app/ticketing/urls.py
@@ -21,4 +21,5 @@ from .import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
+    path('ticket/', include('ticket.urls')),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
